45aeb9a4aaf1b5749c95a638907d73716233952e
ShayFae <ifrahnoor108@gmail.com>
2022-04-27 16:55:16 -0400

    can add to list array

--- src/component/List.js
+++ src/component/List.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
+import "./List.css"
+
 export default function List() {
+  const [list, setList] = useState([])
+  const [add, setAdd] = useState('')
+
+  const addToList = () => {
+    setList([...list, add])
+  }
+
+  const parseList = list.map(lists => <h1>{lists} <br /></h1>)
+
   return(
-    <div>
-      <h1>LIST</h1>
+    <div className="list-section">
+      <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+        <input
+          name="list"
+          type="text"
+          value={add}
+          onChange={(event) => {setAdd(event.target.value)}}
+        />
+        <button onClick={addToList} type="submit">Add</button>
+       </form>
+      {parseList}
     </div>
   );
 }
