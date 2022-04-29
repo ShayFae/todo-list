c029d66620653e98c2772b87ed35c47fb743f112
ShayFae <ifrahnoor108@gmail.com>
2022-04-28 22:49:30 -0400

    Add delete button

--- src/component/List.js
+++ src/component/List.js
@@ -4,12 +4,37 @@ import "./List.css"
 export default function List() {
   const [list, setList] = useState([])
   const [add, setAdd] = useState('')
+  const [u, setU] = useState(false)
 
   const addToList = () => {
     setList([...list, add])
   }
 
-  const parseList = list.map(lists => <h1>{lists} <br /></h1>)
+  //Works but only has one unique ID so un
+  const k = () => {
+    document.getElementById("meep").classList.remove('meep').add('a');
+  }
+
+  //Works but applies to all
+  const j = () => {
+    setU(true)
+  }
+
+  //Make this the id?
+  let x = 0
+
+  const parseList = list.map(lists =>  <h1 className={u ? `a ${lists}` : null} >{lists}<button  
+  onClick={j}  >Delete</button> <br /></h1>, x++ )
+
+  console.log('This is x', x)
+  // const y = list.map(lists => <button onClick={k} >Delete</button> )
+
+  // console.log()
+  
+  // const remove = () => {
+  //   list.filter(x => x === document.getElementsByClassName('find'))
+  //   console.log('yes')
+  // }
 
   return(
     <div className="list-section">
@@ -23,6 +48,7 @@ export default function List() {
         <button onClick={addToList} type="submit">Add</button>
        </form>
       {parseList}
+      <button onClick={k}></button>
     </div>
   );
 }
