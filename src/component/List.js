64d0bac5e69a594ed92255d7853db640bae5b031
ShayFae <ifrahnoor108@gmail.com>
2022-05-04 23:15:16 -0400

    Work on edit feature

--- src/component/List.js
+++ src/component/List.js
@@ -2,19 +2,60 @@ import { useState } from "react";
 import "./List.css"
 
 export default function List() {
-  const [list, setList] = useState([])
-  const [add, setAdd] = useState('')
+  const [list, setList] = useState([]);
+  const [add, setAdd] = useState('');
+  const [count, setCount] = useState(0);
 
   const addToList = () => {
-    setList([...list, add])
+    setList([...list, add]);
+    setCount(prev => prev + 1);
   }
 
+  //Filters matching name attribute 
   const  remove = (event) => {
-    const name = event.target.getAttribute("name")
+    const name = event.target.getAttribute("name");
     setList(list.filter(lists => lists !== name));
   }
 
-  const parseList = list.map(lists => <h1>{lists} <button name={lists} onClick={remove}>Delete</button><br /></h1> )
+  const edit = (event) => {
+    const name = event.target.getAttribute("name");
+    // const test = prompt('change');
+    let c = false;
+    // console.log('this is test', test)
+    // setList(list.filter(lists => { if(lists === name) {
+    //   lists = test;
+    //   console.log('this is lists', lists)
+    //   console.log('yes')
+
+    // }}));
+    // console.log('name', name)
+    //Name is null need to find matching identifer to use conditional then make it equal to the prompt instead and set it to state.
+    setList(list.filter(lists => { if(lists !== name) {
+      c = true;
+      // console.log(lists)
+      // setList(list.filter(g => g !== name));
+      // list.push(test);
+      // console.log('this is lists', lists)
+      // console.log('yes')
+      document.getElementById(lists).innerText = 'testing';
+
+    } }));
+    // console.log('this is lists2', list)
+    if(c) {
+      console.log('true');
+
+      // return console.log(<h1>{test}</h1>)
+    }
+    //Return new element with that state?
+    // return <h1>{test}</h1>
+  }
+
+// const test = (event) => {
+//   const y = document.getElementById('test')
+//   y.innerHTML = event.target.value
+// }
+
+  const parseList = list.map(lists => <h1 id={lists} className={lists} name={lists}> {lists} <button name={lists} onClick={remove}>Delete</button><button onClick={edit}></button><br /></h1> )
 
   return(
     <div className="list-section">
@@ -23,10 +64,10 @@ export default function List() {
           name="list"
           type="text"
           value={add}
-          onChange={(event) => {setAdd(event.target.value)}}
+          onChange={(event) => {setAdd(event.target.value)}}          
         />
         <button onClick={addToList} type="submit">Add</button>
-       </form>
+       </form> 
       {parseList}
     </div>
   );
