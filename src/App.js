be51e4647762dccf845833c79d78950c498b6e4e
ShayFae <ifrahnoor108@gmail.com>
2022-05-16 21:52:40 -0400

    Fix delete icon and add css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import List from './component/List.js'
-import logo from './logo.svg';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faTrashCan, faPencil } from '@fortawesome/free-solid-svg-icons'
 
@@ -9,7 +8,6 @@ library.add(faTrashCan, faPencil)
 function App() {
   return (
     <div className="App">
-      {/* <img src={logo} /> */}
         <h1 className="title"><mark>To-Do List!</mark></h1>
         <List />
     </div>
--- src/component/List.js
+++ src/component/List.js
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import "./List.css"
-// import Test from './docs/blank-notebook-filler-paper-background-a-great-backdrop-to-let-your-creativity-loose-on-this-tiles-seamlessly-as-a-pattern_BYgYIKCBs_thumb.jpg'
-// import Im from './logo.svg'
 import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 
@@ -24,45 +22,10 @@ export default function List() {
     setList(list.filter(lists => lists !== name));
   }
 
-  const edit = (event) => {
-    const name = event.target.getAttribute("name");
-    // const test = prompt('change');
-    let c = false;
-    // console.log('this is test', test)
-    // setList(list.filter(lists => { if(lists === name) {
-    //   lists = test;
-    //   console.log('this is lists', lists)
-    //   console.log('yes')
+  //Messing with the delete function 
+  // <FontAwesomeIcon icon="trash-can"/>
 
-    // }}));
-    // console.log('name', name)
-    //Name is null need to find matching identifer to use conditional then make it equal to the prompt instead and set it to state.
-    // setList(list.filter(lists => { if(lists !== name) {
-    //   c = true;
-    //   // console.log(lists)
-    //   // setList(list.filter(g => g !== name));
-    //   // list.push(test);
-    //   // console.log('this is lists', lists)
-    //   // console.log('yes')
-    //   document.getElementById(lists).innerText = 'testing';
-
-    // } }));
-    // console.log('this is lists2', list)
-    if(c) {
-      alert("HI")
-
-      console.log('true');
-      document.getElementsByName("meep").innerText = 'testing';
-
-      // return console.log(<h1>{test}</h1>)
-    }
-    //Return new element with that state?
-    // return <h1>{test}</h1>
-  }
-
-  <button name="meep" onClick={edit}></button>
-
-  const parseList = list.map(lists => <p id={lists} className={lists} name={lists}> {num++}. {lists} <button name={lists} onClick={remove}> <FontAwesomeIcon icon="trash-can" name={lists} onClick={() => remove}/><br /></button></p>)
+  const parseList = list.map(lists => <p id={lists} className={lists} name={lists}> {num++}. {lists}<button name={lists} onClick={remove}><br/></button></p>)
   return(
     <div >
       <form autoComplete="off" onSubmit={event => event.preventDefault()}>
@@ -76,10 +39,6 @@ export default function List() {
 </button>
        </form> 
        <div className="list-section">
-         {/* <img src={Test} /> */}
-         {/* <FontAwesomeIcon icon="trash-can" /> */}
-         {/* <FontAwesomeIcon icon="trash-can" onClick={() => remove}/> */}
-         {/* <button name={lists} onClick={remove}>Delete</button> */}
       {parseList}
       </div>
     </div>
